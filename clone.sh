da4651e8d5ef8fa75995f20b7d201c58781832da
Ujwala Patel <ujwala.patel@mavenir.com>
2020-12-18 12:33:43 +0530

    initial commit

--- clone.sh
+++ clone.sh
@@ -2,5 +2,5 @@
 git init
 git add .
 git commit -m "initial commit"
-git remote add origin ssh://git@localhost:8081/gitea_admin/platform.git
+git remote add origin ssh://git@localhost:2222/gitea_admin/platform.git
 git push -u origin master
