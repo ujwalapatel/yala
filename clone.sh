652308680665d0ec13ad3d6dd21209268500ca76
Ujwala Patel <ujwala.patel@mavenir.com>
2020-12-21 14:17:40 +0530

    initial commit

--- clone.sh
+++ clone.sh
@@ -3,4 +3,4 @@ git init
 git add .
 git commit -m "initial commit"
 git remote add gitea ssh://git@localhost:2222/gitea_admin/bitbucket.git
-git push -u origin master -f
+git push -u gitea master -f
